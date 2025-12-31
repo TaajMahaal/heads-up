5c0ca2abdfb423e5a5177bc646106c0978a46db1
Mahaal <t.mahaal@proton.me>
2025-12-31 17:10:25 +0100

    part 36

--- lib/heads_up_web/live/admin_incident_live/form.ex
+++ lib/heads_up_web/live/admin_incident_live/form.ex
@@ -4,15 +4,26 @@ defmodule HeadsUpWeb.AdminIncidentLive.Form do
   alias HeadsUp.Admin
   alias HeadsUp.Incidents.Incident
 
-  def mount(_params, _session, socket) do
+  def mount(params, _session, socket) do
+    {:ok, apply_action(socket, socket.assigns.live_action, params)}
+  end
+
+  defp apply_action(socket, :new, _params) do
     changeset = Admin.change_incident(%Incident{})
 
-    socket =
-      socket
-      |> assign(:page_title, "New Incident")
-      |> assign(:form, to_form(changeset))
+    socket
+    |> assign(:page_title, "New Incident")
+    |> assign(:form, to_form(changeset))
+  end
 
-    {:ok, socket}
+  defp apply_action(socket, :edit, %{"id" => id}) do
+    incident = Admin.get_incident!(id)
+    changeset = Admin.change_incident(incident)
+
+    socket
+    |> assign(:page_title, "Edit Incident")
+    |> assign(:form, to_form(changeset))
+    |> assign(:incident, incident)
   end
 
   def handle_event("save", %{"incident" => incident_params}, socket) do
@@ -33,7 +44,7 @@ defmodule HeadsUpWeb.AdminIncidentLive.Form do
   end
 
   def handle_event("validate", %{"incident" => incident_params}, socket) do
-    changeset = Admin.change_incident(%Incident{}, incident_params)
+    changeset = Admin.change_incident(socket.assigns.incident, incident_params)
 
     socket =
       socket
