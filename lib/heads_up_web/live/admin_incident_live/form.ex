855bd0bd56800533079692789bf29388c08973fa
Mahaal <t.mahaal@proton.me>
2025-12-31 11:35:04 +0100

    part 32

--- lib/heads_up_web/live/admin_incident_live/form.ex
+++ lib/heads_up_web/live/admin_incident_live/form.ex
@@ -1,6 +1,8 @@
 defmodule HeadsUpWeb.AdminIncidentLive.Form do
   use HeadsUpWeb, :live_view
 
+  alias HeadsUp.Admin
+
   def mount(_params, _session, socket) do
     socket =
       socket
@@ -8,5 +10,13 @@ defmodule HeadsUpWeb.AdminIncidentLive.Form do
       |> assign(:form, to_form(%{}, as: "incident"))
 
     {:ok, socket}
+  end
+
+  def handle_event("save", %{"incident" => incident_params}, socket) do
+    _incident = Admin.create_incident(incident_params)
+
+    socket = push_navigate(socket, to: ~p"/admin/incidents")
+
+    {:noreply, socket}
   end
 end
