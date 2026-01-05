36c2dd3087f3f6f9d88cfe2f998c0e92a9129596
Mahaal <t.mahaal@proton.me>
2026-01-05 16:34:26 +0100

    part 38

--- lib/heads_up/admin.ex
+++ lib/heads_up/admin.ex
@@ -4,6 +4,10 @@ defmodule HeadsUp.Admin do
 
   import Ecto.Query, warn: false
 
+  def get_incident!(id) do
+    Repo.get!(Incident, id)
+  end
+
   def list_incidents do
     Incident
     |> order_by(desc: :inserted_at)
@@ -20,13 +24,13 @@ defmodule HeadsUp.Admin do
     Incident.changeset(incident, attrs)
   end
 
-  def get_incident!(id) do
-    Repo.get!(Incident, id)
-  end
-
   def update_incident(%Incident{} = incident, attrs) do
     incident
     |> Incident.changeset(attrs)
     |> Repo.update()
+  end
+
+  def delete_incident(incident) do
+    Repo.delete(incident)
   end
 end
--- lib/heads_up_web/live/admin_incident_live/index.ex
+++ lib/heads_up_web/live/admin_incident_live/index.ex
@@ -13,4 +13,25 @@ defmodule HeadsUpWeb.AdminIncidentLive.Index do
 
     {:ok, socket}
   end
+
+  def handle_event("delete", %{"id" => id}, socket) do
+    incident = Admin.get_incident!(id)
+
+    case Admin.delete_incident(incident) do
+      {:ok, incident} ->
+        socket =
+          socket
+          |> stream_delete(:incidents, incident)
+          |> put_flash(:info, "Incident #{incident.name} deleted successfully")
+
+        {:noreply, socket}
+
+      _ ->
+        socket =
+          socket
+          |> put_flash(:error, "Failed to delete incident #{incident.name}")
+
+        {:noreply, socket}
+    end
+  end
 end
